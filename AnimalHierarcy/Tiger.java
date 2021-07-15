7a91662e3ffadbe16b36a91e762eb8726367ae7e
PiyushPatil3099 <73695958+PiyushPatil3099@users.noreply.github.com>
2021-07-15 10:33:42 +0530

    Update Tiger.java

--- AnimalHierarcy/Tiger.java
+++ AnimalHierarcy/Tiger.java
@@ -4,13 +4,13 @@ public class Tiger extends feline implements Hunt {
 	@Override
 	void makeNoise() {
 		// TODO Auto-generated method stub
-		super.makeNoise();
+		
 		System.out.println("Tiger makes snarl noise");
 	}
 	@Override
 	void eat() {
 		// TODO Auto-generated method stub
-		super.eat();
+		
 		System.out.println("Tiger eats meat");
 	}
 	@Override
