29d602b1848685c312c1be9237dd3a2c0b8962b8
PiyushPatil3099 <73695958+PiyushPatil3099@users.noreply.github.com>
2021-07-15 10:36:17 +0530

    Update Hippo.java

--- AnimalHierarcy/Hippo.java
+++ AnimalHierarcy/Hippo.java
@@ -10,13 +10,18 @@ public class Hippo extends Animal implements Swimming{
 	@Override
 	void makeNoise() {
 		// TODO Auto-generated method stub
-		super.makeNoise();
+		
 		System.out.println("Hippo makes growl sound");
 	}
 	@Override
 	public void swimmingAbility() {
 		// TODO Auto-generated method stub
 		System.out.println("Hippo poses swimming ability");
+		
+	}
+	@Override
+	void roam() {
+		// TODO Auto-generated method stub
 		
 	}
 
