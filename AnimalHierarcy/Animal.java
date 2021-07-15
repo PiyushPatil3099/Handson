6ea4b7dbdbb905e94ce2f0e545ae6b40e69a1b14
PiyushPatil3099 <73695958+PiyushPatil3099@users.noreply.github.com>
2021-07-15 10:27:24 +0530

    Update Animal.java

--- AnimalHierarcy/Animal.java
+++ AnimalHierarcy/Animal.java
@@ -1,20 +1,14 @@
 package AnimalHierarcy;
-public class Animal {
+abstract public class Animal {
 	String food;
 	String hunger;
 	int boundries;
 	String location;
 	String image;
-	void makeNoise() {
-		System.out.println("Animal Makes Noise");
-	}
-	void eat() {
-		System.out.println("Animal can eat");
-	}
+	abstract void makeNoise();
+	abstract void eat();
 	void sleep() {
 		System.out.println("Animal can sleep");
 	}
-	void roam() {
-		System.out.println("Animal can roam");
-	}
+	abstract void roam();
 }
