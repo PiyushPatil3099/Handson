9031ca3a07f979682c1433e6b5cb68cd3d6da2d6
PiyushPatil3099 <73695958+PiyushPatil3099@users.noreply.github.com>
2021-07-15 10:32:45 +0530

    Update Lion.java

--- AnimalHierarcy/Lion.java
+++ AnimalHierarcy/Lion.java
@@ -4,13 +4,13 @@ public class Lion extends feline implements Hunt{
 	@Override
 	void makeNoise() {
 		// TODO Auto-generated method stub
-		super.makeNoise();
+		
 		System.out.println("Lion makes roars noise");
 	}
 	@Override
 	void eat() {
 		// TODO Auto-generated method stub
-		super.eat();
+		
 		System.out.println("Lion eats meat");
 	}
 	@Override
